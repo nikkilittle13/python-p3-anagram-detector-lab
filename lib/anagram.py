8f484d1b2477144c777434e3dd1a4a3bafe93a91
Nikki Little <nikkilittle13@gmail.com>
2024-04-17 15:09:08 -0400

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word
+  
+  def match(self, word_list):
+    matches = []
+
+    for word in word_list:
+      if sorted(list(word.lower())) == sorted(list(self.word.lower())):
+        matches.append(word)
+
+    return matches
+    
+
+
+
+    
